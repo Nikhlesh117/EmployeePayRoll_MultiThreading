c2d6b52cb40646230266b63baba35b8566a1311b
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-03-23 16:41:18 +0530

    Added: Add Multiple Employee

---
+++ EmployeePayroll.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollMultiThreading
+{
+    public class EmployeePayroll
+    {
+        public List<EmployeeDetails> employeePayrollDetailsList = new List<EmployeeDetails>();
+        // UC1- Without Use of Thread
+        public void AddEmployeeToPayroll(List<EmployeeDetails> employeePayrollDataList)
+        {
+            employeePayrollDataList.ForEach(employeeData =>
+            {
+                Console.WriteLine("Employee being added: " + employeeData.Name);
+                this.AddEmployeePayroll(employeeData);
+                Console.WriteLine("Employee added: " + employeeData.Name);
+            });
+            Console.WriteLine(this.employeePayrollDetailsList.Count());
+        }
+        public void AddEmployeePayroll(EmployeeDetails emp)
+        {
+            employeePayrollDetailsList.Add(emp);
+        }
+
+    }
+}

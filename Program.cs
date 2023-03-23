8b592118a01bab9ecbbfd28ad3db14f25cce9859
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-03-23 10:17:07 +0530

    Welcome To The Employee Payroll Multi Threading

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeePayrollMultiThreading
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Employee Payroll Multi Threading");
+        }
+    }
+}
